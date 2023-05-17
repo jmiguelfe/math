da0711e97be6edebc3a0ed826d4e6d348691319e
jmiguelfe <125206196+jmiguelfe@users.noreply.github.com>
2023-05-17 19:05:10 +0100

    new

---
+++ README.md
@@ -0,0 +1,11 @@
+The purpose of this project is for you to calculate the following:
+
+Average
+Median
+Variance
+Standard Deviation
+
+Instructions
+
+Firt downloading and run script with ./math-skills (this script does a general number rando and saves the out put in the data.txt file.)
+Run the script with ./math-skills, then run the program with the created file (data.txt) go run main.go 
--- main.go
+++ main.go
@@ -1,90 +1,99 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"log"
 	"math"
 	"os"
 	"sort"
 	"strconv"
-	"strings"
 )
 
-func calculateMedian(data []int) int {
-	// Sort the data in ascending order
-	sort.Ints(data)
-
-	length := len(data)
-	if length%2 == 0 {
-		// If the length is even, average the middle two elements
-		middle1 := data[length/2-1]
-		middle2 := data[length/2]
-		return (middle1 + middle2) / 2
-	} else {
-		// If the length is odd, return the middle element
-		middle := data[length/2]
-		return middle
-	}
-}
-// The function calculates the average of a given slice of integers.
-func calculateAverage(data []int) int {
-	sum := 0
-	for _, value := range data {
-		sum += value
-	}
-	return sum / len(data)
-}
-// The function calculates the variance of a given set of integers.
-func calculateVariance(data []int) int {
-	average := calculateAverage(data)
-	sumOfSquares := 0
-	for _, value := range data {
-		deviation := value - average
-		sumOfSquares += deviation * deviation
-	}
-	return sumOfSquares / len(data)
-}
-// The function calculates the standard deviation of a given set of integer data.
-func calculateStandardDeviation(data []int) int {
-	variance := calculateVariance(data)
-	return int(math.Round(math.Sqrt(float64(variance))))
-}
-// The function reads data from a file, calculates the median, average, variance, and standard
-// deviation of the data, and prints the results.
 func main() {
-	if len(os.Args) < 2 {
-		log.Fatal("Please provide a file path as an argument.")
-	}
-
-	filePath := os.Args[1]
-
-	content, err := os.ReadFile("data.txt")
+	number, err := ReadFile("data.txt")
 	if err != nil {
-		log.Fatalf("Failed to read file: %s", err)
+		panic(err)
 	}
 
-	lines := strings.Split(string(content), "\n")
-	data := make([]int, 0, len(lines))
+	Average := CalculateAverage(number)
+	Median := CalculateMedian(number)
+	Variance := CalculateVariance(number)
+	StandardDeviation := CalculateStandardDeviation(number)
 
-	for _, line := range lines {
-		line = strings.TrimSpace(line)
-		if line == "" {
-			continue
-		}
+	fmt.Printf("Average: %.0f\n", Average)
+	fmt.Printf("Median: %0.f\n", Median)
+	fmt.Printf("Variance: %0.f\n", Variance)
+	fmt.Printf("StandardDeviation: %0.f\n", StandardDeviation)
+}
 
-		value, err := strconv.Atoi(line)
+func CalculateAverage(numbers []float64) float64 {
+	sumatoria := 0.0
+	for _, valor := range numbers {
+		sumatoria += valor
+	}
+	avarage := sumatoria / float64(len(numbers))
+	return avarage
+}
+
+func CalculateMedian(numbers []float64) float64 {
+	sort.Float64s(numbers) // Sorte the number in ascending order
+	leng := len(numbers)   // Get the length of the slice
+
+	if leng%2 == 0 {
+		// check if the lenght is even Or par
+		return (numbers[leng/2-1] + numbers[leng/2]) / 2
+	}
+	// se deveria de aver puesto +1 pero como en array se empieza por zero nose pone
+	return numbers[leng/2]
+}
+
+func CalculateStandardDeviation(numbers []float64) float64 {
+	//  la desviación estándar de la población es igual a la raíz cuadrada de la varianza:
+	Variance := CalculateVariance(numbers)
+
+	stanndarD := math.Sqrt(float64(Variance))
+	return stanndarD
+}
+
+func CalculateVariance(numbers []float64) float64 {
+	// Get the avarage
+	average := CalculateAverage(numbers)
+	sum := 0.0
+	for _, numb := range numbers {
+		sum += math.Pow(float64(numb)-float64(average), 2)
+	}
+	result := sum / float64(len(numbers))
+
+	return result
+}
+
+func ReadFile(fileName string) ([]float64, error) {
+	// Open the file
+	file, err := os.Open(fileName)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var numbers []float64
+	scanner := bufio.NewScanner(file)
+
+	// Read each line of the file
+	for scanner.Scan() {
+		line := scanner.Text()
+		// Convert the text from each line to a float64
+		number, err := strconv.ParseFloat(line, 64)
 		if err != nil {
-			log.Fatalf("Invalid data format in file: %s", filePath)
+			return nil, err
 		}
-
-		data = append(data, value)
+		// Append the converted number to the slice
+		numbers = append(numbers, number)
 	}
-	median := calculateMedian(data)
-	average := calculateAverage(data)
-	variance := calculateVariance(data)
-	standardDeviation := calculateStandardDeviation(data)
-	fmt.Println("Median:", median)
-	fmt.Println("Average:", average)
-	fmt.Println("Variance:", variance)
-	fmt.Println("Standard Deviation:", standardDeviation)
+
+	// Check for any scanning errors
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return numbers, nil
 }
